82a5845ed8923a3e9aff39b3ce4cb834c6e3501b
royhoo <cnroyhoo@gmail.com>
2023-03-26 14:27:31 +0800

    Initial commit

---
+++ sophomore/NFT-Collection/constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0x5C946e1c53AE0E7989Fb816c0c2271b679E3839c";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://learn-web3-dao-nft-collection-app.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };

e52ca6aa8a0e39c5efa5c05a2d5bf187b13c175d
royhoo <cnroyhoo@gmail.com>
2023-04-01 18:13:43 +0800

    add ICO

---
+++ ICO/constants/index.js
@@ -0,0 +1,4 @@
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS = "0x006E87E3D6c4013b2EaC393D426BaaE04fAb0556";
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
